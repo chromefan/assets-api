18f36b94541d5abe7cf6d39487fe2c1ec844ee60
luohuanjun <luohuanjun@baidu.com>
2020-01-05 10:09:36 +0800

    first
    Change-Id: I45208eaabf56987c0c57b44628128b4ec5d67102

--- assets.sql
+++ assets.sql
@@ -41,6 +41,16 @@ CREATE TABLE `assets` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `assets`
+--
+
+LOCK TABLES `assets` WRITE;
+/*!40000 ALTER TABLE `assets` DISABLE KEYS */;
+INSERT INTO `assets` VALUES (10,'cash',76627.00,76800.00,173.00,0.23,34.61,'招商银行活期，余额宝','',1578152366,1578152366,0),(16,'gold',32000.00,35200.00,3200.00,10.00,14.45,'积存金，博士黄金ETF','',1578130828,1578130828,0),(17,'btc',20000.00,8000.00,-12000.00,-60.00,9.03,'比特币','',1578152273,1578152273,0),(18,'stock',63000.00,53605.00,-9395.00,-14.91,28.46,'哔哩哔哩，小米集团','',1578152990,1578152990,0),(19,'fund',29774.00,30817.64,1044.00,3.51,13.45,'广发聚鑫债券，兴全社会责任混合','',1578152703,1578152703,0);
+/*!40000 ALTER TABLE `assets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `assets_log`
 --
 
@@ -63,6 +73,16 @@ CREATE TABLE `assets_log` (
   KEY `type_name` (`type_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `assets_log`
+--
+
+LOCK TABLES `assets_log` WRITE;
+/*!40000 ALTER TABLE `assets_log` DISABLE KEYS */;
+INSERT INTO `assets_log` VALUES (10,'cash',76627.00,76800.00,173.00,0.23,34.61,'招商银行活期，余额宝','',1578152366,0),(16,'gold',32000.00,35200.00,3200.00,10.00,14.45,'积存金，博士黄金ETF','',1578130828,0),(17,'btc',20000.00,8000.00,-12000.00,-60.00,9.03,'比特币','',1578152273,0),(18,'stock',63000.00,53605.00,-9395.00,-14.91,28.46,'哔哩哔哩，小米集团','',1578152990,0),(19,'fund',29773.00,30817.64,1044.64,3.51,13.45,'广发聚鑫债券，兴全社会责任混合','',1578130873,0);
+/*!40000 ALTER TABLE `assets_log` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -73,4 +93,4 @@ CREATE TABLE `assets_log` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-05 10:06:43
+-- Dump completed on 2020-01-05 10:08:33
